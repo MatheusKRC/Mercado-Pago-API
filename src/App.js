e0db68073d90e03fc852c4a3795c8524e1969b97
Matheus leão <kyracraft0@gmail.com>
2023-09-15 14:59:54 -0300

    Iniciando Projeto Api Mercado Pago

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CSS/App.css";
+import Home from "./Pages/Home";
+
+// const api = axios.create({
+//   baseURL: "https://api.mercadopago.com"
+// });
+
+function App() {
+  return (
+    <Home/>
+  );
+}
+
+export default App;
